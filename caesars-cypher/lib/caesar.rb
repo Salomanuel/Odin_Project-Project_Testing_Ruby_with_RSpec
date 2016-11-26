09ff72d60f49e3b91aa26b0242b82d7a29ed9f8d
Manuel <salomanuel.aux+code@gmail.com>
2016-11-26 13:02:57 +0100

    the damn old caesar file is back

--- caesars-cypher/lib/caesar.rb
+++ caesars-cypher/lib/caesar.rb
@@ -1,61 +1,45 @@
+ #caesar_cipher("What a string!", 5)
+ #  => "Bmfy f xywnsl!"
+
 class CaesarCypher
  def caesar(string, length=5)
-	alphabet      = ('a'..'z').to_a
-	downcase      = ("a".."z").to_a
-	upcase        = ("A".."Z").to_a
-	alphabet_size = upcase.length
+	alphabet = ('a'..'z').to_a
 	result = []
-	
 
-	return string.split("").map do |char|
-		case
-		when char =~ /!|\.|,|:|\s/ then char #puts "punct"
-		when char == char.upcase
-			# puts "upcase"
-			char_index = upcase.index(char)
-			if (char_index + length ) > alphabet_size
-				  char = upcase[((char_index + length) - alphabet_size)]
-			else
-				  char = upcase[  char_index + length]
-			end
-		when char == char.downcase
-			# puts "downcase"
-			char_index = downcase.index(char)
-			if (char_index + length ) > alphabet_size
-				char = downcase[((char_index + length) - alphabet_size)]
-			else
-				char = downcase[  char_index + length]
-			end
-		end
-	end.join
+ 	string.downcase.split("").each do |char|
+ 		if char == " " or char == "!"
+ 			new_char = char
+ 		elsif (alphabet.index(char)+length) > alphabet.length
+ 			new_char = alphabet[(alphabet.index(char)+length) - alphabet.length]
+ 		else
+ 			new_char = alphabet[alphabet.index(char)+length]	
+ 		end
+ 		result.push new_char
+ 	end
+
+ 	#case size
+ 	string.split("").each_with_index do |char, j|
+ 	  if char == " " or char == "!"
+ 	  elsif char == char.upcase
+ 	  	result[j] = result[j].upcase
+ 	  end
+ 	end
+
+ 	return result.join
+
+ end
 end
 
-end
+#puts caesar("AaAA")
+#puts caesar("What")
+#puts caesar("ciaoz", 5)
+# puts caesar("What a string!", 5)
 
 
- 	# string.downcase.split("").each do |char|
- 	# 	if char == " " or char == "!"
- 	# 		new_char = char
- 	# 	elsif (alphabet.index(char)+length) > alphabet.length
- 	# 		new_char = alphabet[(alphabet.index(char)+length) - alphabet.length]
- 	# 	else
- 	# 		new_char = alphabet[alphabet.index(char)+length]	
- 	# 	end
- 	# 	result.push new_char
- 	# end
 
- 	# #case size
- 	# string.split("").each_with_index do |char, j|
- 	#   if char == " " or char == "!"
- 	#   elsif char == char.upcase
- 	#   	result[j] = result[j].upcase
- 	#   end
- 	# end
-
- 	# return result.join
-
-#  end
-# end
-
-# uoo = CaesarCypher.new
-# puts uoo.caesar("abba")
+=begin
+text.each do |char|
+ 		old_value = (char.ord() + length)
+ 		new_value.push old_value.chr
+ 	end
+=end
